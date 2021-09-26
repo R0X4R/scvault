f6d58b2bbd517b3f710c02d1f15c7fb8c88fbeb7
Eshan Singh <32596297+R0X4R@users.noreply.github.com>
2021-09-26 15:36:30 +0530

    `subenum.sh`

--- subenum.sh
+++ subenum.sh
@@ -6,6 +6,7 @@ fi
 ffuf -u http://FUZZ.$1/ -t 150 -mc 200 -fl 0 -sf -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36" -w ~/wordlists/subdomains.txt -p '0.6-1.2' -maxtime 900 -o ffuf.json 2> /dev/null &> /dev/null
 cat ffuf.json | jq -r '.results[] | .host' 2> /dev/null | anew subdomains.txt
 gobuster dns -d $1 --no-error -z -q -t 50 -w ~/wordlists/subdomains.txt | sed 's/Found: //g' | anew subdomains.txt
+xargs -a wordlists/subdomains.txt -P 20 -I @ bash -c "echo \"@.$1\" | dnsx -silent -t 120 -retry 2 -r wordlists/resolvers.txt"
 timeout 50m amass enum -active -brute -w ~/wordlists/subdomains.txt -d $1 -o amass.txt &> /dev/null
 cat amass.txt | anew subdomains.txt
 rm -rf ffuf.json amass.txt
